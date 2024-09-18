afa9add1883a34ae5aa2ad1ad5288ae3412f3c6e
joyal0 <76865773+joyal0@users.noreply.github.com>
2024-09-18 05:51:18 +0530

    Python(Revision) 18Sep24

---
+++ Learn Python/5_Testing_and_debugging/LearningEffectively4_quiz.py
@@ -0,0 +1,36 @@
+print("You will be penalized for peeking at the \
+	solution after you have successfully completed \
+	an assignment. \n True \n False(My Choice)")
+
+
+
+
+
+
+"""
+Learning Effectively
+
+ This course is about to get a bit harder. There's no
+ way around it, if programming were a walk in the park 
+ everyone would be earning 6 figures as a software 
+ engineer. But it's not, and to succeed without getting 
+ stuck and frustrated, you need to learn how to learn.
+
+Process for solving hard coding problems
+
+1.	Read the lesson first! Figure out the examples before writing your own code.
+2.	Read the assignment. Understand the goal of the assignment before you start writing code.
+3.	Start writing code.
+4.	Add print() statements. Don't wait until you've written a lot of code to start testing. Add print() statements and use the Run button to see if your code is doing what you expect at each step. It's easier to find issues in small bits of code than in large blocks of code.
+	•	Keep running, printing, and fixing until you're confident your code is working.
+5.	Submit your code. If the assignment you're working on has unit tests, no need to remove your debugging print() statements. If the assignment you're working on is testing console output, be sure to remove your print() statements before submitting.
+6.	Compare your code to the instructor's. You will not be penalized for looking at the solution after you have successfully completed the assignment.
+
+Additional tidbits
+
+•	Try to use Boots before peeking at the solution. Boots is quite good at giving you pointed hints to help you solve the problem on your own.
+•	It's okay to peek at the solution when you're completely stuck every once in a while, but don't make it a habit. If you find that you're always stuck, you should restart the chapter or course to make sure you understand the material.
+•	You can reset your code for an assignment with the "reset" button. For example, maybe you forgot which modifications you made vs which code was left by the instructor.
+•	You can reset all your cached code from the settings page. This is useful if you want to restart a course or chapter.
+
+"""
---
+++ Learn Python/5_Testing_and_debugging/TwoLessonTypes2_quiz.py
@@ -0,0 +1,20 @@
+"""Two Lesson Types
+As we talked about, there are 2 types of coding lessons on Boot.dev:
+
+	Console output lessons
+	Unit-test lessons
+Console output lessons
+
+*Only 1 file of code, usually with a comment explaining where to write your code
+*When you "submit" your code, its console output must match the expected output exactly to pass
+*Debug print statements will cause your code to fail submission, so remove them before submitting
+
+Unit-test lessons
+
+*2 files of code: main and main_test. You can read the tests but you can't edit them
+*When you "submit" your code, the return values of your functions must match the expected values exactly to pass
+*Console output is ignored, you can leave debug print statements in your code
+"""
+
+print("You can submit ____ lessons with your debugging code. \
+		\n Unit test(My Choice) \n Console Output")
